df2194d18070e03a0b4de17845cd94d9b6ac8a20
rifrifqyqy <rifqyh22@gmail.com>
2024-05-23 11:58:20 +0700

    responsive herosection, about section

--- src/components/Layouts/AboutSection.jsx
+++ src/components/Layouts/AboutSection.jsx
@@ -106,28 +106,28 @@ export default function AboutSection() {
   return (
     <>
       <main className="relative" id="about">
-        <div className="grid grid-cols-2 mx-32 mt-24 ">
-          <section className="my-auto">
-            <motion.div variants={""} initial="initial" whileInView="view" className="flex flex-col max-w-fit items-end">
+        <div className="grid grid-cols-2 max-md:grid-cols-1 mx-32 max-md:mx-8 mt-24 max-md:mt-8 ">
+          <section className="my-auto max-md:order-2">
+            <motion.div variants={""} initial="initial" whileInView="view" className="flex flex-col max-w-fit items-end max-md:mt-4">
               <motion.div className="relative">
-                <motion.h1 variants={textShow} className="font-statliches text-[48px] leading-tight">
+                <motion.h1 variants={textShow} className="font-statliches text-[48px] max-md:text-[24px] leading-tight">
                   RIFQY HAMDANI
                 </motion.h1>
-                <motion.div variants={coverAnimate} className="w-[120%] h-[90px] bg-sky-700 absolute z-10 top-0 left-0"></motion.div>
+                <motion.div variants={coverAnimate} className="w-[120%] h-[90px] max-md:h-[32px] bg-sky-700 absolute z-10 top-0 left-0"></motion.div>
               </motion.div>
-              <motion.h1 variants={textShow} className="font-statliches text-[20px] text-white bg-sky-700 max-w-fit px-6 mt-[-8px]">
+              <motion.h1 variants={textShow} className="font-statliches text-[20px] max-md:text-[14px] text-white bg-sky-700 max-w-fit px-6 max-md:px-2 mt-[-8px] max-md:mt-[-4px]">
                 21 Years old
               </motion.h1>
             </motion.div>
             <motion.div variants={""} initial="initial" whileInView="view" className="relative">
-              <motion.p variants={textShow} className="mt-8 w-[570px] text-slate-500">
+              <motion.p variants={textShow} className="mt-8 max-md:mt-4 w-[570px] max-md:w-fit text-slate-500 max-md:text-[14px]">
                 "Passionate about art and design, Rifqy Hamdani is an enthusiastic college student pursuing his dreams in the world of design. With a fondness for UI design and a keen interest in UX research, Rifqy brings dedication and
                 creativity to every project he faces. With arts thinking and logic as his foundation, he delves into the complexities of frontend web design diligently, crafting captivating user experiences and stunning aesthetics.
               </motion.p>
               <motion.div variants={coverAnimate} className="bg-sky-700 w-[110%] h-full absolute top-0 left-0 z-10"></motion.div>
             </motion.div>
 
-            <div className="w-[350px] mx-auto mt-16 gap-8 overflow-hidden whitespace-nowrap">
+            <div className="w-[350px] max-md:w-[300px] mx-auto mt-16 max-md:mt-4 gap-4 overflow-hidden whitespace-nowrap">
               <div class="marquee py-5">
                 <div class="marquee-content">
                   <span className="flex gap-4">
@@ -138,7 +138,7 @@ export default function AboutSection() {
                     <img src="images/skill/photoshop.svg" alt="" />
                   </span>
                 </div>
-                <div class="marquee-content" aria-hidden="true">
+                <div class="marquee-content max-md:pl-8" aria-hidden="true">
                   <span className="flex gap-4">
                     <img src="images/skill/framermotion.svg" alt="" />
                     <img src="images/skill/framer.svg" alt="" />
@@ -150,7 +150,7 @@ export default function AboutSection() {
               </div>
               {/* marquee2 */}
               <div class="marquee py-5">
-                <div class="marquee-content">
+                <div class="marquee-content code">
                   <span className="flex gap-5">
                     <img src="images/skill/react.svg" alt="" />
                     <img src="images/skill/vitejs.svg" alt="" />
@@ -159,7 +159,7 @@ export default function AboutSection() {
                     <img src="images/skill/tailwind.svg" alt="" />
                   </span>
                 </div>
-                <div class="marquee-content" aria-hidden="true">
+                <div class="marquee-content code max-md:pl-8" aria-hidden="true">
                   <span className="flex gap-5">
                     <img src="images/skill/react.svg" alt="" />
                     <img src="images/skill/vitejs.svg" alt="" />
@@ -171,25 +171,25 @@ export default function AboutSection() {
               </div>
             </div>
           </section>
-          <section className="">
+          <section className=" max-md:order-1">
             <div className="relative">
-              <motion.div variants={imgAnimate} initial="initial" whileInView="view" onMouseEnter="" onMouseLeave="" className="ml-16">
-                <motion.img onMouseEnter={handleHover} onMouseLeave={handleMouseLeave} src={img} alt="" className="transition duration-500" />
-                <motion.img variants={russo} src="images/russo.svg" alt="" className="absolute bottom-0 -z-10 left-16" />
+              <motion.div variants={imgAnimate} initial="initial" whileInView="view" onMouseEnter="" onMouseLeave="" className="ml-16 max-md:ml-0">
+                <motion.img onMouseEnter={handleHover} onMouseLeave={handleMouseLeave} src={img} alt="" className="transition duration-500 max-md:h-[300px] max-md:mx-auto" />
+                <motion.img variants={russo} src="images/russo.svg" alt="" className="absolute bottom-0 -z-10 left-16 max-md:left-0 max-md:right-0 max-md:mx-auto max-md:h-[330px]" />
               </motion.div>
 
-              <motion.div variants={boxAnimate} initial="initial" whileInView="view" className="w-[300px] h-[120px] bg-sky-100 absolute top-[50%] right-0 flex overflow-y-hidden">
-                <p className="font-statliches text-white bottom-0 right-0 text-2xl bg-sky-700 px-6 absolute z-10">RIFQY HAMDANI</p>
+              <motion.div variants={boxAnimate} initial="initial" whileInView="view" className="w-[300px] max-md:w-[150px] h-[120px] max-md:h-[60px] bg-sky-100 absolute top-[50%] right-0 flex overflow-y-hidden">
+                <p className="font-statliches text-white bottom-0 right-0 text-2xl max-md:text-[14px] max-md:h-[16px] max-md:w-[80px] text-nowrap bg-sky-700 px-6 max-md:px-1 leading-[120%] absolute z-10">RIFQY HAMDANI</p>
                 <div className=" h-[100%] w-[100%] relative overflow-hidden">
-                  <motion.img variants={boxImg} src="images/seongcrop.svg" alt="" className="absolute top-0 left-[-24px] scale-[180%]" />
+                  <motion.img variants={boxImg} src="images/seongcrop.svg" alt="" className="absolute top-0 left-[-24px] max-md:left-[-8px] scale-[180%]" />
                 </div>
               </motion.div>
             </div>
           </section>
         </div>
-        <aside className="absolute top-[40%] left-0 -z-20">
+        <aside className="absolute top-[40%] max-md:top-[-100px] left-0 -z-20">
           <img src="images/dotpattern.png" alt="" />
-          <img src="images/text-vertical.png" alt="" className="ml-16 -mt-24" />
+          <img src="images/text-vertical.png" alt="" className="ml-16 -mt-24 max-md:ml-4" />
         </aside>
       </main>
     </>
--- src/components/Layouts/HeroSection.jsx
+++ src/components/Layouts/HeroSection.jsx
@@ -54,30 +54,44 @@ export default function HeroSection() {
       },
     },
   };
+  const wrapAnimateMobile = {
+    initial: {
+      width: "310px",
+    },
+    view: {
+      width: 0,
+      transition: {
+        duration: 0.5,
+        delay: 1,
+      },
+    },
+  }
   //   animate logic end
 
   return (
     <>
-      <motion.section variants={homeAnimate} ref={constraintsRef} whileInView="view" initial="initial" animate="animate" className=" relative overflow-hidden " id="home">
-        <div className="grid grid-cols-2 max-md:grid-cols-1">
-          <motion.img variants={imageAnimate} drag dragConstraints={constraintsRef} src="images/liluo.svg" alt="" className="w-[70%] mx-40" />
+      <motion.section variants={homeAnimate} ref={constraintsRef} whileInView="view" initial="initial" animate="animate" className=" relative overflow-hidden max-md:py-2 " id="home">
+        <div className="grid grid-cols-2 max-md:grid-cols-1 max-md:mx-8">
+          <motion.img variants={imageAnimate} drag dragConstraints={constraintsRef} src="images/liluo.svg" alt="" className="w-[70%] mx-40 max-md:mx-4" />
           <motion.div variants={titleAnimate} className="my-auto">
             <p className="font-statliches">welcome to my site</p>
-            <h1 className="text-[180px] relative leading-[100%] font-valorant tracking-[20px] -ml-2">
+            <h1 className="text-[180px] max-md:text-[52px] relative leading-[100%] font-valorant tracking-[20px] -ml-2 max-md:ml-0">
               <span className="text-sky-700">L</span>I<span className="text-sky-700">L</span>UO
-              <motion.div variants={wrapAnimate} className="h-[145px] mr-40 absolute top-0 right-0 bg-sky-800 z-20"></motion.div>
+              <motion.div variants={wrapAnimate} className="h-[145px] max-md:h-[50px] mr-40 max-md:mr-0 absolute top-0 right-0 max-md:left-0 bg-sky-700 z-20 max-md:hidden"></motion.div>
+              <motion.div variants={wrapAnimateMobile} className="h-[45px] absolute top-0 right-0 max-md:left-0 bg-sky-700 z-20 hidden max-md:block"></motion.div>
             </h1>
-            <div className="relative bg-slate-600">
-              <p className="font-statliches mt-[-24px] text-2xl">"The man who failed to enter the Faculty of Fine Arts and Design."</p>
-              <motion.div variants={wrapAnimate} className="h-[32px] mr-40 absolute top-0 right-0 bg-sky-800 z-20"></motion.div>
+            <div className="relative">
+              <p className="font-statliches mt-[-24px] max-md:mt-0 text-2xl max-md:text-lg max-md:leading-tight">"The man who failed to enter the Faculty of Fine Arts and Design."</p>
+              <motion.div variants={wrapAnimate} className="h-[32px] mr-40 absolute top-0 right-0 bg-sky-700 z-20 max-md:hidden"></motion.div>
+              <motion.div variants={wrapAnimateMobile} className="h-[45px] absolute top-0 left-0 bg-sky-700 z-20 max-md:block hidden"></motion.div>
             </div>
 
-            <div className="mt-4">
-              <MainButton className=" py-3 bg-slate-800 text-xl ">Start Journey</MainButton>
+            <div className="mt-4 max-md:mt-2">
+              <MainButton className="py-3 max-md:py-2 bg-slate-800 text-xl max-md:text-[14px] max-md:px-4 ">Start Journey</MainButton>
             </div>
           </motion.div>
         </div>
-        <div className="w-[100%] h-12 border-[1px] border-gray-800 absolute top-96 left-0 rotate-45 -z-10">
+        <div className="w-[100%] max-md:w-[600px] h-12 border-[1px] border-gray-800 absolute top-96 max-md:top-40 left-0 max-md:-left-20 rotate-45 -z-10">
           <marquee className="pt-2 opacity-30" scrollamount="20" loop="-1">
             Rifqy Hamdani. 21 years old Enthusiast in design
             <span className="mx-96">Rifqy Hamdani. 21 years old Enthusiast in design </span>
@@ -86,7 +100,7 @@ export default function HeroSection() {
             <span className="mx-96">Rifqy Hamdani. 21 years old Enthusiast in design </span>
           </marquee>
         </div>
-        <div className="w-[2200px] flex justify-center h-12 bg-gray-800 opacity-30 -rotate-[15deg] translate-x-28 absolute top-72 right-0 -z-10"></div>
+        <div className="w-[2200px] max-md:w-[600px] flex justify-center h-12 bg-gray-800 opacity-30 -rotate-[15deg] max-md:rotate-[-30deg] translate-x-28 absolute top-72 max-md:top-40 right-0 -z-10"></div>
 
         <div className="w-[100vw] overflow-x-hidden text-[280px] text-white -ml-24  translate-x-28 opacity-60 absolute top-[30%] -z-20">
           <img src="images/EGAMEDIEV.svg" alt="" />
